fabf0be63cd441638ab8a42a68c299b6bb9697e2
Leonardo <leo_narita@hotmail.com>
2021-07-25 22:17:25 -0300

    Configurando Redis + Desetivando devtools (erro conversão classe)

--- src/main/java/br/com/ifsp/pi/lixt/utils/security/config/AuthServerConfig.java
+++ src/main/java/br/com/ifsp/pi/lixt/utils/security/config/AuthServerConfig.java
@@ -5,6 +5,8 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.provider.token.DefaultTokenServices;
+import org.springframework.security.oauth2.provider.token.TokenStore;
 
 import br.com.ifsp.pi.lixt.utils.security.oauth.OauthManager;
 import br.com.ifsp.pi.lixt.utils.security.oauth.server.OauthUserDetailsService;
@@ -17,20 +19,22 @@ public class AuthServerConfig extends AuthorizationServerConfigurerAdapter {
 	private final OauthManager oauthManager;
 	private final PasswordEncoder passwordEncoder;
 	private final OauthUserDetailsService userDetailsService;
+	private final TokenStore tokenStore;
+	private final DefaultTokenServices tokenServices;
 	
 	@Override
 	public void configure(AuthorizationServerEndpointsConfigurer endpoints) {
-		endpoints.authenticationManager(oauthManager).userDetailsService(userDetailsService);
+		endpoints.tokenStore(tokenStore).tokenServices(tokenServices).authenticationManager(oauthManager).userDetailsService(userDetailsService);
 	}
 
 	@Override
 	public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
 		clients.inMemory()
-        	.withClient("client")
-        	.authorizedGrantTypes("password", "authorization_code", "refresh_token", "client_credentials").scopes("all")
-        	.refreshTokenValiditySeconds(300000)
-        	.secret(passwordEncoder.encode("123456"))
-        	.accessTokenValiditySeconds(50000);
+	        	.withClient("client")
+	        	.authorizedGrantTypes("password", "authorization_code", "refresh_token", "client_credentials").scopes("all")
+	        	.refreshTokenValiditySeconds(300000)
+	        	.secret(passwordEncoder.encode("123456"))
+	        	.accessTokenValiditySeconds(50000);
 	}
 
 }
--- src/main/java/br/com/ifsp/pi/lixt/utils/security/oauth/BeansConfig.java
+++ src/main/java/br/com/ifsp/pi/lixt/utils/security/oauth/BeansConfig.java
@@ -2,22 +2,46 @@ package br.com.ifsp.pi.lixt.utils.security.oauth;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.provider.token.DefaultTokenServices;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.redis.RedisTokenStore;
 
 @Configuration
 public class BeansConfig {
 	
 	private final PasswordEncoder passwordEncoder;
+	private final TokenStore tokenStore;
+	private final DefaultTokenServices tokenServices;
 	
-	public BeansConfig() {
+	public BeansConfig(RedisConnectionFactory redisConnectionFactory) {
 
 		passwordEncoder = new BCryptPasswordEncoder();
+		tokenStore = new RedisTokenStore(redisConnectionFactory);
+		tokenServices = new DefaultTokenServices();
+
+		tokenServices.setTokenStore(tokenStore);
+		tokenServices.setSupportRefreshToken(true);
+		tokenServices.setAccessTokenValiditySeconds(60 * 60);
 	}
 
 	@Bean
 	public PasswordEncoder passwordEncoder() {
 		return passwordEncoder;
+	}
+	
+	@Bean
+	public TokenStore tokenStore() {
+		return tokenStore;
+	}
+	
+	@Bean
+	@Primary
+	public DefaultTokenServices tokenServices() {
+	    return tokenServices;
 	}
 
 }
