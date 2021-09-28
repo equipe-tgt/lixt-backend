2c6907f4c1b810bc39ac259e25edc7ee23e906a4
Fabio Mendes <fabiomendes53@gmail.com>
2021-09-28 09:25:59 -0300

    refactor: adjust on swagger of list of items dto

--- src/main/java/br/com/ifsp/pi/lixt/dto/ListOfItemsDto.java
+++ src/main/java/br/com/ifsp/pi/lixt/dto/ListOfItemsDto.java
@@ -2,6 +2,7 @@ package br.com.ifsp.pi.lixt.dto;
 
 import java.util.List;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -15,17 +16,21 @@ import lombok.NoArgsConstructor;
 @EqualsAndHashCode
 public class ListOfItemsDto {
 
+	@ApiModelProperty(hidden = true)
 	private Long id;
 	
 	private String nameList;
 	
 	private Long ownerId;
-	
+
+	@ApiModelProperty(hidden = true)
 	private String owner;
 	
 	private String description;
-	
+
+	@ApiModelProperty(hidden = true)
 	private List<ProductOfListDto> productsOfList;
-	
+
+	@ApiModelProperty(hidden = true)
 	private List<ListMembersDto> listMembers;
 }
