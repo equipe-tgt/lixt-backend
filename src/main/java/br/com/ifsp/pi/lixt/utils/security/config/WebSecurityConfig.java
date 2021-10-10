c64770cbdbb63ccbc65c00155abd6f07a42d59fe
Alkindar Ferraz <alferraz@uolinc.com>
2021-10-10 18:19:03 -0300

    Mover para heroku

--- src/main/java/br/com/ifsp/pi/lixt/utils/security/config/WebSecurityConfig.java
+++ src/main/java/br/com/ifsp/pi/lixt/utils/security/config/WebSecurityConfig.java
@@ -1,5 +1,6 @@
 package br.com.ifsp.pi.lixt.utils.security.config;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
@@ -7,18 +8,18 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import lombok.RequiredArgsConstructor;
+import org.springframework.security.web.header.writers.ReferrerPolicyHeaderWriter;
 
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 @RequiredArgsConstructor
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-	
+
 	private static final String[] AUTH_WHITELIST = {
 			"/swagger-resources/**", "/swagger-ui.html", "/v2/api-docs", "/webjars/**"
 	};
-	
+
 	@Override
 	public void configure(WebSecurity web) throws Exception {
 		web.ignoring()
@@ -29,9 +30,19 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 				.antMatchers(AUTH_WHITELIST)
 				.antMatchers(HttpMethod.OPTIONS,"/**");
 	}
-	
+
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
+		http.requiresChannel()
+				.requestMatchers(r -> r.getHeader("X-Forwarded-Proto") != null)
+				.requiresSecure();
+
+		http.headers()
+				.httpStrictTransportSecurity().maxAgeInSeconds(31536000).includeSubDomains(true).and()
+				.contentSecurityPolicy("hello").and()
+				.referrerPolicy(ReferrerPolicyHeaderWriter.ReferrerPolicy.SAME_ORIGIN).and()
+				.permissionsPolicy().policy("geolocation=(self, \"https://lixt-ws.azure.net\")");
+
 		http.authorizeRequests()
 			.antMatchers(AUTH_WHITELIST).permitAll()
 			.antMatchers("/auth/register").permitAll()
@@ -43,5 +54,5 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 			.csrf().disable()
 			.httpBasic();
 	}
-	
+
 }
