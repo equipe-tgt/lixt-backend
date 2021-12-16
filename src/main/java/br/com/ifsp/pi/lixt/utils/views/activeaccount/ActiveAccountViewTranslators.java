8fd9ee7c2897427b277277132a97f32655f6e3e2
Carolina de Moraes Josephik <carolina.moraesjosephik@gmail.com>
2021-12-16 12:21:04 -0300

    inserindo link de tutorial da plataforma na view de conta ativada

--- src/main/java/br/com/ifsp/pi/lixt/utils/views/activeaccount/ActiveAccountViewTranslators.java
+++ src/main/java/br/com/ifsp/pi/lixt/utils/views/activeaccount/ActiveAccountViewTranslators.java
@@ -14,6 +14,9 @@ public abstract class ActiveAccountViewTranslators {
 		
 		params.put("$TITLE", "Conta Ativada com Sucesso!");
 		params.put("$RESPONSE", "Agora você tem acesso garantido à plataforma Lixt! 😀");
+		params.put("$TUTORIAL", "Em dúvida de como dar seus primeiros passos?");
+		params.put("$LINK_TEXT", "Veja este vídeo");
+		params.put("$LINK", "https://youtu.be/FVS_pm66l20");
 		params.put("$WARNING", "Esperamos que aproveite cada experiência, planejar um compra pode ser tão divertido quanto comprar!");
 
 		return params;
@@ -24,6 +27,9 @@ public abstract class ActiveAccountViewTranslators {
 		
 		params.put("$TITLE", "Account Activated Successfully!");
 		params.put("$RESPONSE", "You are now guaranteed access to the Lixt platform! 😀");
+		params.put("$TUTORIAL", "In doubt about how to take your first steps?");
+		params.put("$LINK_TEXT", "Watch this video");
+		params.put("$LINK", "https://youtu.be/FVS_pm66l20");
 		params.put("$WARNING", "We hope you enjoy every experience, planning a purchase can be as fun as shopping!");
 		
 		return params;
